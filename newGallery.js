1b5cdbe99076191ffae2f2f08f38febdc4927efd
luvlestudio <sowoojoo31@gmail.com>
2024-10-05 10:01:38 +0900

    메인변경

--- newGallery.js
+++ newGallery.js
@@ -47,7 +47,7 @@ window.onload = () => {
       content: {
         title: "송낙현 ♥ 최윤정의 모바일 청첩장",
         description: "결혼식에 초대합니다♡",
-        imageUrl: "https://i.imgur.com/EcPItqc.png",
+        imageUrl: "https://i.imgur.com/FjevQ38.png",
         imageWidth: 1200,
         imageHeight: 630,
         link: {
